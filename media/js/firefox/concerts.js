56bcdcc38922bd182a6fe97da7b70f706f03eea9
Craig Cook <craigcook@users.noreply.github.com>
2018-11-28 16:57:07 -0800

    Update non-US header, widen FxA

--- media/js/firefox/concerts.js
+++ media/js/firefox/concerts.js
@@ -15,146 +15,6 @@
     var className;
     var state;
 
-
-    /*
-        This function initiates the OAuth process.
-
-        It looks for a state key stored in a cookie, and, if not found, generates a new
-        key and places it in a cookie.
-
-        The state value is then set in the form and on the direct link URL.
-
-        Next, we retrieve metrics information from an asynchronous FxA call. When this
-        call completes, the submit button on the form is enabled.
-    */
-    function initOauth() {
-        var fxaFormWrapper = document.getElementById('fxa-form-wrapper');
-        var metricsFlowEndpoint = fxaFormWrapper.getAttribute('data-fxa-metrics-endpoint');
-        var flowIdField = document.getElementById('flow_id');
-        var flowBeginTimeField = document.getElementById('flow_begin_time');
-        var stateField = document.getElementById('state');
-        var fxaSignIn = document.getElementById('fxa-sign-in');
-
-        // check for an existing state value in a cookie
-        if (Mozilla.Cookies.hasItem(stateStorageKey)) {
-            state = Mozilla.Cookies.getItem(stateStorageKey);
-        } else {
-            // generate a state token and put it in a cookie
-
-            // the suffix below is superfluous for now, but will help us identify
-            // different OAuth flows in the future
-            state = Math.random() + '_concert_q42018';
-
-            var d = new Date();
-            d.setTime(d.getTime() + (cookieDays * 24 * 60 * 60 * 1000));
-            Mozilla.Cookies.setItem(stateStorageKey, state, d.toUTCString(), '/');
-        }
-
-        // put state value in form & append to sign-in link
-        stateField.value = state;
-        fxaSignIn.href += '&state=' + state;
-
-        // get metrics flow stuff from FxA
-        fetchTokens(metricsFlowEndpoint);
-
-        // get tokens from FxA for analytics purposes
-        // copied from base/mozilla-fxa-form.js
-        function fetchTokens(destURL) {
-            // add required params to the token fetch request
-            destURL += '?utm_campaign=firefox-concert-series-q4-2018&utm_source=mozilla.org';
-
-            fetch(destURL).then(function(resp) {
-                return resp.json();
-            }).then(function(r) {
-                flowIdField.value = r.flowId;
-                flowBeginTimeField.value = r.flowBeginTime;
-            }).catch(function() {
-                // silently fail, leaving flow_id and flow_begin_time as default empty value
-            });
-        }
-    }
-
-    // update class based on cookie state so we can show/hide the appropriate content
-
-    // if user has a verified cookie, we can skip the OAuth flow
-    if (Mozilla.Cookies.hasItem(verifiedStorageKey)) {
-        className = 'is-verified';
-    } else {
-        className = 'not-verified';
-        initOauth();
-    }
-
-    // apply className determined above to show proper content
-    document.documentElement.className += ' ' + className;
-
-
-    // Calculate time from now to a future endtime
-    function getTimeRemaining(endtime) {
-        var t = Date.parse(endtime) - Date.parse(new Date());
-        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-        var days = Math.floor(t / (1000 * 60 * 60 * 24));
-
-        return {
-            'total': t,
-            'days': days,
-            'hours': hours
-        };
-    }
-
-    // Initialize the countdown timer
-    function initializeClock(id, endtime) {
-        var clock = document.getElementById(id);
-        var daysSpan = clock.querySelector('.days');
-        var hoursSpan = clock.querySelector('.hours');
-
-        function updateClock() {
-            var t = getTimeRemaining(endtime);
-
-            daysSpan.innerHTML = t.days;
-            hoursSpan.innerHTML = t.hours;
-
-            if (t.total <= 0) {
-                clearInterval(timeinterval);
-            }
-        }
-
-        updateClock();
-        var timeinterval = setInterval(updateClock, 60000);
-    }
-
-    if (countdownOne) {
-        initializeClock(countdownOne, showtimeOne);
-    }
-
-    var content = document.querySelector('.mzp-u-modal-content');
-    var trigger = document.querySelector('.email-privacy-link');
-    var title = document.querySelector('.email-privacy h3');
-
-    trigger.addEventListener('click', function(e) {
-        e.preventDefault();
-        Mzp.Modal.createModal(e.target, content, {
-            title: title.innerHTML,
-            className: 'mzp-t-firefox',
-            closeText: window.Mozilla.Utils.trans('global-close'),
-        });
-
-        window.dataLayer.push({
-            'event': 'in-page-interaction',
-            'eAction': 'link click',
-            'eLabel': 'How will Mozilla use my email?'
-        });
-    }, false);
-
-    // Update utm_content to indicate browser
-    var utmContent = document.getElementById('utm_content');
-
-    if (window.Mozilla.Client.isFirefox) {
-        utmContent.value = 'firefox';
-    } else {
-        utmContent.value = 'not-firefox';
-    }
-
-    // Check geolocation
     var ConcertPage = {
         US_COUNTRY_CODE: 'us',
         COOKIE_ID: 'firefox-concerts',
@@ -239,11 +99,12 @@
     };
 
     ConcertPage.showConcertContent = function() {
-        // you can see the show
+        // Add styling hook for us-specific CSS.
+        document.body.classList.add('state-in-us');
     };
 
     ConcertPage.showExcludedContent = function() {
-        // Add styling hook for excluded specific CSS.
+        // Add styling hook for excluded-specific CSS.
         document.body.classList.add('state-not-us');
     };
 
@@ -299,8 +160,147 @@
             ConcertPage.showConcertContent();
         }
     };
-
     window.Mozilla.ConcertPage = ConcertPage;
+
+
+    /*
+        This function initiates the OAuth process.
+
+        It looks for a state key stored in a cookie, and, if not found, generates a new
+        key and places it in a cookie.
+
+        The state value is then set in the form and on the direct link URL.
+
+        Next, we retrieve metrics information from an asynchronous FxA call. When this
+        call completes, the submit button on the form is enabled.
+    */
+    function initOauth() {
+        var fxaFormWrapper = document.getElementById('fxa-form-wrapper');
+        var metricsFlowEndpoint = fxaFormWrapper.getAttribute('data-fxa-metrics-endpoint');
+        var flowIdField = document.getElementById('flow_id');
+        var flowBeginTimeField = document.getElementById('flow_begin_time');
+        var stateField = document.getElementById('state');
+        var fxaSignIn = document.getElementById('fxa-sign-in');
+
+        // check for an existing state value in a cookie
+        if (Mozilla.Cookies.hasItem(stateStorageKey)) {
+            state = Mozilla.Cookies.getItem(stateStorageKey);
+        } else {
+            // generate a state token and put it in a cookie
+
+            // the suffix below is superfluous for now, but will help us identify
+            // different OAuth flows in the future
+            state = Math.random() + '_concert_q42018';
+
+            var d = new Date();
+            d.setTime(d.getTime() + (cookieDays * 24 * 60 * 60 * 1000));
+            Mozilla.Cookies.setItem(stateStorageKey, state, d.toUTCString(), '/');
+        }
+
+        // put state value in form & append to sign-in link
+        stateField.value = state;
+        fxaSignIn.href += '&state=' + state;
+
+        // get metrics flow stuff from FxA
+        fetchTokens(metricsFlowEndpoint);
+
+        // get tokens from FxA for analytics purposes
+        // copied from base/mozilla-fxa-form.js
+        function fetchTokens(destURL) {
+            // add required params to the token fetch request
+            destURL += '?utm_campaign=firefox-concert-series-q4-2018&utm_source=mozilla.org';
+
+            fetch(destURL).then(function(resp) {
+                return resp.json();
+            }).then(function(r) {
+                flowIdField.value = r.flowId;
+                flowBeginTimeField.value = r.flowBeginTime;
+            }).catch(function() {
+                // silently fail, leaving flow_id and flow_begin_time as default empty value
+            });
+        }
+    }
+
+    // update class based on cookie state so we can show/hide the appropriate content
+
+    // if user has a verified cookie, we can skip the OAuth flow
+    if (Mozilla.Cookies.hasItem(verifiedStorageKey)) {
+        className = 'is-verified';
+    } else {
+        className = 'not-verified';
+        initOauth();
+    }
+
+    // apply className determined above to show proper content
+    document.body.classList.add(className);
+
+
+    // Calculate time from now to a future endtime
+    function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        var days = Math.floor(t / (1000 * 60 * 60 * 24));
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours
+        };
+    }
+
+    // Initialize the countdown timer
+    function initializeClock(id, endtime) {
+        var clock = document.getElementById(id);
+        var daysSpan = clock.querySelector('.days');
+        var hoursSpan = clock.querySelector('.hours');
+
+        function updateClock() {
+            var t = getTimeRemaining(endtime);
+
+            daysSpan.innerHTML = t.days;
+            hoursSpan.innerHTML = t.hours;
+
+            if (t.total <= 0) {
+                clearInterval(timeinterval);
+            }
+        }
+
+        updateClock();
+        var timeinterval = setInterval(updateClock, 60000);
+    }
+
+    if (countdownOne) {
+        initializeClock(countdownOne, showtimeOne);
+    }
+
+    var content = document.querySelector('.mzp-u-modal-content');
+    var trigger = document.querySelector('.email-privacy-link');
+    var title = document.querySelector('.email-privacy h3');
+
+    trigger.addEventListener('click', function(e) {
+        e.preventDefault();
+        Mzp.Modal.createModal(e.target, content, {
+            title: title.innerHTML,
+            className: 'mzp-t-firefox',
+            closeText: window.Mozilla.Utils.trans('global-close'),
+        });
+
+        window.dataLayer.push({
+            'event': 'in-page-interaction',
+            'eAction': 'link click',
+            'eLabel': 'How will Mozilla use my email?'
+        });
+    }, false);
+
+    // Update utm_content to indicate browser
+    var utmContent = document.getElementById('utm_content');
+
+    if (window.Mozilla.Client.isFirefox) {
+        utmContent.value = 'firefox';
+    } else {
+        utmContent.value = 'not-firefox';
+    }
+
 
 
 
